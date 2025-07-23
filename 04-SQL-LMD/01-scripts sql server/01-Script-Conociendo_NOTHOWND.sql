8a72f0cb8ad658a1782fe26c2d28d1bd2bac176a
Alfredo Juan Moroni <alfredogit26@gmail.com>
2025-07-22 23:03:33 -0600

    Logro:Codigos terminados

---
+++ 04-SQL-LMD/01-scripts sql server/01-Script-Conociendo_NOTHOWND.sql
@@ -0,0 +1,27 @@
+USE NORTHWND;
+
+SELECT*
+FROM Categories;
+
+SELECT *
+FROM Products;
+
+
+SELECT *
+FROM Customers;
+
+
+SELECT *
+FROM Employees;
+
+
+SELECT *
+FROM Shippers;
+
+
+SELECT *
+FROM Orders;
+
+
+SELECT *
+FROM [Order Details];
